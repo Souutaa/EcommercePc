40feaaad9b01d27a4f83ba9e69d322737e1b7504
Quang Long <trinhq011@gmail.com>
2023-10-23 14:27:07 +0700

    bug infinity when find account_id in table account detail

--- backend/src/main/java/com/app/ecommerce/controllers/UserController.java
+++ backend/src/main/java/com/app/ecommerce/controllers/UserController.java
@@ -14,6 +14,8 @@ import org.springframework.web.server.ResponseStatusException;
 import com.app.ecommerce.DTO.accountDetail.CreateAccountDetailDTO;
 import com.app.ecommerce.DTO.accountDetail.UpdateAccountDetailDTO;
 import com.app.ecommerce.models.Account;
+import com.app.ecommerce.models.AccountDetail;
+import com.app.ecommerce.services.IAccountDetailServices;
 import com.app.ecommerce.services.IAccountServices;
 
 import lombok.RequiredArgsConstructor;
@@ -24,51 +26,17 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
     @Autowired
     private IAccountServices accountServices;
+    @Autowired
+    private IAccountDetailServices accountDetailServices;
 
-    @GetMapping(value = "/getUser")
-    public @ResponseBody ResponseEntity<Object> getAllAccount(@RequestParam String id) {
-        Account test = accountServices.getAccountById(Integer.parseInt(id));
-        return new ResponseEntity<>(test, HttpStatus.OK);
+    @GetMapping(value = "/getUser/{username}")
+    public @ResponseBody ResponseEntity<Object> getAllAccount(@RequestParam String id, @PathVariable String username) {
+        try {
+            Account test = accountServices.getAccountById(Integer.parseInt(id));
+            return new ResponseEntity<>(test, HttpStatus.OK);
+        } catch (ResourceNotFoundException ex) {
+            throw new ResourceNotFoundException("user not found");
+        }
     }
 
-    @GetMapping(value = "/allDetailUser")
-    public @ResponseBody ResponseEntity<Object> getAllAccountDetail() {
-        List<AccountDetail> accountDetail = accountDetailServices.getAllAccountDetails();
-        return new ResponseEntity<Object>(accountDetail, HttpStatus.OK);
-    }
-
-    @GetMapping(value = "/allDetailUser/active")
-    public @ResponseBody ResponseEntity<Object> getAllAccountDetailActive() {
-        List<AccountDetail> accountDetail = accountDetailServices.getAccountDetailActive();
-        return new ResponseEntity<Object>(accountDetail, HttpStatus.OK);
-    }
-
-    @GetMapping(value = "/{id}")
-    public @ResponseBody ResponseEntity<Object> getAccountDetail(@PathVariable String id) {
-        AccountDetail accountDetail = accountDetailServices.getAccountDetailById(Integer.parseInt(id));
-        return new ResponseEntity<Object>(accountDetail, HttpStatus.OK);
-    }
-
-    @PostMapping(value = "/createUserDetail")
-    public ResponseEntity<AccountDetail> CreateAccountDetailDTO(
-            @RequestBody CreateAccountDetailDTO createAccountDetailDTO) {
-        return ResponseEntity.ok(accountDetailServices.saveAccountDetail(createAccountDetailDTO));
-    }
-
-    @PatchMapping(value = "/updateUserDetail")
-    public ResponseEntity<AccountDetail> UpdateAccountDetailDTO(
-            @RequestParam String id, @RequestBody UpdateAccountDetailDTO updateAccountDetailDTO) {
-        return ResponseEntity.ok(accountDetailServices.updateAccountDetail(id, updateAccountDetailDTO));
-    }
-
-    // @DeleteMapping(value = "/deleteUserDetail")
-    // public void deleteAccountDetail(@RequestParam String id) {
-    // accountDetailServices.deleteAccountDetail(Integer.parseInt(id));
-    // }
-
-    // softDelete
-    @DeleteMapping(value = "/deleteUserDetail")
-    public void deleteAccountDetail(@RequestParam String id) {
-        accountDetailServices.softDeleteAcouuAccountDetail(Integer.parseInt(id));
-    }
 }
