e8aeb053ddfd0c6b0b4904fd049c7195113e647f
Shikigami Maito <shikigamimaito25@gmail.com>
2023-10-26 16:03:23 +0700

    remember to create a DTO

---
+++ backend/src/main/java/com/app/ecommerce/DTO/brand/CreateBrandRequest.java
@@ -0,0 +1,14 @@
+package com.app.ecommerce.DTO.brand;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CreateBrandRequest {
+  private String brandName;
+}
