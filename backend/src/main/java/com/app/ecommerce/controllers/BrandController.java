aca4d5e31144f54eddc941bc128a41c4dac2a050
Quang Long <trinhq011@gmail.com>
2023-10-26 14:09:07 +0700

    brand

---
+++ backend/src/main/java/com/app/ecommerce/controllers/BrandController.java
@@ -0,0 +1,46 @@
+package com.app.ecommerce.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.ecommerce.models.Brand;
+import com.app.ecommerce.services.IBrandServices;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController // This means that this class is a Controller
+@RequestMapping(value = "/brand") // This means URL's start with /demo (after Application path)
+@RequiredArgsConstructor
+public class BrandController {
+
+    @Autowired
+    private IBrandServices brandServices;
+
+    @GetMapping(value = "/allBrand")
+    public @ResponseBody ResponseEntity<Object> getAllBrand() {
+        List<Brand> listBrand = brandServices.getAllBrands();
+        return new ResponseEntity<Object>(listBrand, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/getBrand")
+    public @ResponseBody ResponseEntity<Object> getAllBrand(@RequestParam String name) {
+        Brand listBrand = brandServices.getBrandbyName(name);
+        return new ResponseEntity<Object>(listBrand, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/createBrand")
+    public ResponseEntity<Brand> createBrand(
+            @RequestBody Brand brand) {
+        return ResponseEntity.ok(brandServices.saveBrand(brand));
+    }
+}
