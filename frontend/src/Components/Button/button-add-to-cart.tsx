18e0cfb5c8bd8ec078c9aced80c8299d3632327c
vanluongdevz2 <luongdubai0175@gmail.comm>
2023-11-15 11:27:31 +0700

    Buttonmore do product list

--- frontend/src/Components/Button/button-add-to-cart.tsx
+++ frontend/src/Components/Button/button-add-to-cart.tsx
@@ -1,23 +1,28 @@
-import { MantineProvider } from "@mantine/core";
+// import { MantineProvider } from "@mantine/core";
 import { Notifications, notifications } from "@mantine/notifications";
 import Btn from ".";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function ButtonAdd() {
+  const handleClick = () => {
+    toast.success("Đây là thông báo thành công!", {
+      position: "top-right",
+      autoClose: 3000, // Đóng sau 3 giây
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
   return (
     <>
-      <MantineProvider>
-        <Notifications />
-        <Btn
-          maintine="a"
-          onClick={() =>
-            notifications.show({
-              message: "Đã thêm sản phẩm vào giỏ hàng",
-            })
-          }
-        >
-          Thêm vào giỏ
-        </Btn>
-        <Notifications />
-      </MantineProvider>
+      <Notifications />
+      <Btn maintine="a" onClick={handleClick}>
+        Thêm vào giỏ
+      </Btn>
+      <Notifications />
     </>
   );
 }
