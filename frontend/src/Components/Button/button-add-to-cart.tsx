9f6f7fd028aeacd1fb56dfa9cfa7ccdab9c1b133
vanluongdevz2 <luongdubai0175@gmail.comm>
2023-11-15 12:06:10 +0700

    fix drop menu

--- frontend/src/Components/Button/button-add-to-cart.tsx
+++ frontend/src/Components/Button/button-add-to-cart.tsx
@@ -1,28 +1,17 @@
 // import { MantineProvider } from "@mantine/core";
-import { Notifications, notifications } from "@mantine/notifications";
+// import { Notifications, notifications } from "@mantine/notifications";
+import { Button } from "@mantine/core";
 import Btn from ".";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 function ButtonAdd() {
   const handleClick = () => {
-    toast.success("Đây là thông báo thành công!", {
-      position: "top-right",
-      autoClose: 3000, // Đóng sau 3 giây
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
+    toast("Đây là thông báo thành công!");
   };
 
   return (
     <>
-      <Notifications />
-      <Btn maintine="a" onClick={handleClick}>
-        Thêm vào giỏ
-      </Btn>
-      <Notifications />
+      <Button onClick={handleClick}>Thêm vào giỏ</Button>
     </>
   );
 }
--- frontend/src/Layouts/Header.tsx
+++ frontend/src/Layouts/Header.tsx
@@ -1,12 +1,17 @@
-import {
-  Avatar,
-  Autocomplete,
-} from "@mantine/core";
-import { IconSearch } from "@tabler/icons-react";
+import { Avatar, Autocomplete, Menu } from "@mantine/core";
+import { Button, Text, rem } from "@mantine/core";
 import "../Pages/HomePage/style.css";
 import { Link } from "react-router-dom";
 import React, { useState } from "react";
 import Btn from "../Components/Button";
+import {
+  IconSettings,
+  IconSearch,
+  IconPhoto,
+  IconMessageCircle,
+  IconTrash,
+  IconArrowsLeftRight,
+} from "@tabler/icons-react";
 import InputSearch from "../Components/Input/input-search";
 import Seaparator from "../Components/Seaparator/Seaparator";
 import UserInfor from "../Components/UserInfor/UserInfor";
@@ -14,11 +19,11 @@ import UserOder from "../Components/UserOrder/UserOrder";
 import { PATHS } from "../Constants/path";
 
 const Header = () => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [opened, setOpened] = useState(false);
+  // const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
+  // const toggleDropdown = () => {
+  //   setIsDropdownOpen(!isDropdownOpen);
 
   return (
     <div className="header">
@@ -41,10 +46,76 @@ const Header = () => {
                 </button>
               </div>
             </Link>
-            <div className="logo-user" onClick={toggleDropdown}>
-              <Avatar src="/img/Avatar.png" alt="it's me" />
-            </div>
-            {isDropdownOpen && (
+            <Menu opened={opened} onChange={setOpened}>
+              <Menu.Target>
+                <Button>Toggle menu</Button>
+              </Menu.Target>
+              <Menu.Dropdown>
+                <Menu.Label>Application</Menu.Label>
+                <Menu.Item
+                  leftSection={
+                    <IconSettings style={{ width: rem(14), height: rem(14) }} />
+                  }
+                >
+                  Settings
+                </Menu.Item>
+                <Menu.Item
+                  leftSection={
+                    <IconMessageCircle
+                      style={{ width: rem(14), height: rem(14) }}
+                    />
+                  }
+                >
+                  Messages
+                </Menu.Item>
+                <Menu.Item
+                  leftSection={
+                    <IconPhoto style={{ width: rem(14), height: rem(14) }} />
+                  }
+                >
+                  Gallery
+                </Menu.Item>
+                <Menu.Item
+                  leftSection={
+                    <IconSearch style={{ width: rem(14), height: rem(14) }} />
+                  }
+                  rightSection={
+                    <Text size="xs" c="dimmed">
+                      ⌘K
+                    </Text>
+                  }
+                >
+                  Search
+                </Menu.Item>
+
+                <Menu.Divider />
+
+                <Menu.Label>Danger zone</Menu.Label>
+                <Menu.Item
+                  leftSection={
+                    <IconArrowsLeftRight
+                      style={{ width: rem(14), height: rem(14) }}
+                    />
+                  }
+                >
+                  Transfer my data
+                </Menu.Item>
+                <Menu.Item
+                  color="red"
+                  leftSection={
+                    <IconTrash style={{ width: rem(14), height: rem(14) }} />
+                  }
+                >
+                  Delete my account
+                </Menu.Item>
+              </Menu.Dropdown>
+
+              <div className="logo-user">
+                <Avatar src="/img/Avatar.png" alt="it's me" />
+              </div>
+            </Menu>
+
+            {/* {isDropdownOpen && (
               <div className="dropdown-menu">
                 <UserInfor />
                 <Seaparator />
@@ -52,7 +123,7 @@ const Header = () => {
                 <Seaparator />
                 <div className="user-logout">Sign Out</div>
               </div>
-            )}
+            )} */}
           </div>
         </div>
       </div>
