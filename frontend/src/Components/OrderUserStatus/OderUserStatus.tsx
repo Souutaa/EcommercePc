1c372f6fa0cddb26103eb94a4c4a8eea4262b527
Quang Long <trinhq011@gmail.com>
2023-11-22 12:41:26 +0700

    data res is string

--- frontend/src/Components/OrderUserStatus/OderUserStatus.tsx
+++ frontend/src/Components/OrderUserStatus/OderUserStatus.tsx
@@ -2,21 +2,19 @@ import { Link } from "react-router-dom";
 import Seaparator from "../Seaparator/Seaparator";
 import { useEffect, useState } from "react";
 import axios from "axios";
-
-
-
-
+import { AccountOrders } from "../../Pages/OrderUser/OrderUser";
+import formatPrice from "../../Helper/formatPrice";
 import { PATHS } from "../../Constants/path";
 
-function OderUserStatus() {
+function OderUserStatus(props: AccountOrders) {
   return (
     <>
       <div className="user-oder-list">
-        <p className="user-order-id"># 35 </p>
-        <p className="user-order-name">testuser1 </p>
-        <p className="user-order-date-order">2023-05-06 </p>
-        <p className="user-order-total-price">29,490,000đ</p>
-        <p className="user-order-status color-red">Đã Hủy</p>
+        <p className="user-order-id"># {props.id} </p>
+        <p className="user-order-name">{props.username} </p>
+        <p className="user-order-date-order">{props.createAt.getUTCDate()} </p>
+        <p className="user-order-total-price">{formatPrice(props.total)}</p>
+        <p className="user-order-status color-red">{props.status}</p>
         <Link
           to={PATHS.INFORDER}
           className="user-order"
