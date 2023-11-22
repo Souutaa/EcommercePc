d457ca8de57fedf8a38ea25e5d353c947c15b65d
vanluongdevz2 <luongdubai0175@gmail.comm>
2023-11-22 11:16:01 +0700

    Admin Dashborad Product search chi tiet hoa don

--- frontend/src/Pages/OrderUser/OrderUser.tsx
+++ frontend/src/Pages/OrderUser/OrderUser.tsx
@@ -10,7 +10,7 @@ import OderUserStatus from "../../Components/OrderUserStatus/OderUserStatus";
 import UserInfor from "../../Components/UserInfor/UserInfor";
 import UserOder from "../../Components/UserOrder/UserOrder";
 import OderUserStatus from "../../Components/OrderUserStatus/OderUserStatus";
-import Breadcrumbs from "../../Components/Breadcrumbs/Breadcrumb";
+import Breadcrumbs from "../../Components/Breadcrumbs/Breadcrumbs";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
--- frontend/src/Pages/ProductDetail/ProductDetail.tsx
+++ frontend/src/Pages/ProductDetail/ProductDetail.tsx
@@ -18,11 +18,6 @@ type ProductInfo = {
   productInformation: string;
 };
 
-type ProductInfo = {
-  id: number;
-  productInformation: string;
-};
-
 type ProductDetail = {
   brandId: number;
   categoryId: number;
